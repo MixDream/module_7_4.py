b483f763dafc449816b75e4e85c4a04eb79df8a3
MixDream <max_juldashev@mail.ru>
2024-08-08 14:28:12 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,16 @@
+team1_num = 5
+team2_num = 6
+score1 = 40
+score2 = 42
+team1_time = 18015.2
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 350.4
+challenge_result = 'победа команды Волшебники данных!'
+print("В команде Мастера кода участников: %d !" % team1_num)
+print("Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num))
+print("Команда Волшебники данных решила задач: {} !".format(score2))
+print(" Волшебники данных решили задачи за {} с !".format(team1_time))
+print(f"Команды решили {score1} и {score2} задач.")
+print(f'Результат битвы: {challenge_result}')
+print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg:.1f} секунды на задачу! ")
